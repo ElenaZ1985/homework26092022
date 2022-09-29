d7af1dee1924d8eb5d2f6ed939800fe48ac48ace
ElenaZ1985 <zima_szn@list.ru>
2022-09-29 14:08:55 +0400

    25zadacha

---
+++ Desktop/моя/homework260922/zadacha_25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Используя определение степени числа, напишите цикл, который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+Console.WriteLine("введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+int res = 1;
+while (i<b+1)
+{
+res = res * a;
+i++;
+}
+Console.WriteLine(res);
